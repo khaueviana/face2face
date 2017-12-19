5229c2133903c981d8cad6ce64fe9fdd55e5d1fa
adriano.bulcao <adriano.bulcao@concrete.com.br>
2017-12-19 18:10:40 -0200

    Conexão mongoDB.

---
+++ src/db.js
@@ -0,0 +1,20 @@
+var options = {
+    useMongoClient: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  };
+
+  var uri = "mongodb://face2face:sohosarrombados@tnis6.mongodb.net:27017/concrete-shard-0";
+  mongoose.connect(uri, options);
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    email: String
+}, { collection: 'usercollection' }
+);
+ 
+module.exports = { Mongoose: mongoose, UserSchema: userSchema }
