28f0965808eee1e79adba400baa5e6bde2ac6e36
khaue.viana <khaue.viana@concrete.com.br>
2017-12-20 16:32:58 -0200

    Add Bcrypt

--- models/user.js
+++ models/user.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String
 }, { timestamps: true } );
 
-mongoose.model("User", userSchema);
+UserSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+mongoose.model("User", UserSchema);
