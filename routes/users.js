3e6cf472de03f065420eda9a6b78e64901aba64f
adriano.bulcao <adriano.bulcao@concrete.com.br>
2017-12-20 15:44:32 -0200

    CRUD User - Padrão Restful.

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/getById/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   User.findById(req.params.id).then(function(user) {
     return res.send(user);
   }).catch(function(e) {
@@ -20,7 +20,7 @@ router.get('/getById/:id', function(req, res, next) {
   });
 });
 
-router.post('/create', function(req, res, next) {
+router.post('/', function(req, res, next) {
   var user = new User();
   user.username = req.body.username;
   user.email = req.body.email;
@@ -30,6 +30,23 @@ router.post('/create', function(req, res, next) {
     return res.sendStatus(200);
   }).catch(function(e) {
     return res.sendStatus(404);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  var id = req.params.id;
+
+  var user = new User();
+  user._id = id;
+  user.username = req.body.username;
+  user.email = req.body.email;
+  user.password = req.body.password;
+
+  User.findByIdAndUpdate(id, user).then(function(user) {
+    return res.send(user);
+  }).catch(function(e) {
+    console.log(e);
+    return res.send('Not Found');
   });
 });
 
