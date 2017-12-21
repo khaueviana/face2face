6b3b419a77a4a3259a35dc87d028259c2e114ebf
khaue.viana <khaue.viana@concrete.com.br>
2017-12-21 12:06:13 -0200

    Refactor

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,8 +17,8 @@ app.use(cookieParser());
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://face2face:sohosarrombados@concrete-shard-00-00-tnis6.mongodb.net:27017,concrete-shard-00-01-tnis6.mongodb.net:27017,concrete-shard-00-02-tnis6.mongodb.net:27017/test?ssl=true&replicaSet=concrete-shard-0&authSource=admin');
 
-var userModel = require('./models/user');
-var userModel = require('./models/game');
+require('./models/user');
+require('./models/game');
 
 app.use(function (req, res, next) {
 
@@ -42,20 +43,16 @@ app.use('/', require('./controllers/index'));
 app.use('/users', require('./controllers/users'));
 app.use('/games', require('./controllers/games'));
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
 });
 
