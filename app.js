485ec9dc836ff20b64f69d102553d64c95e8f29e
celonet <marcelo.lopesdasilva@gggmail.com>
2017-12-22 15:23:13 -0200

    change

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(cors({
   preflightContinue: false,
   optionsSuccessStatus: 200
 }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
