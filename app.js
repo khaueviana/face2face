bcf9017479c6362dbfb8b66c5286e05b200a64ab
celonet <marcelo.lopesdasilva@gggmail.com>
2017-12-22 14:47:25 -0200

    modify cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://face-2-face-ui.herokuapp.com/"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
