e0e22eddac0f46332da5c1999ccc089010180fed
celonet <marcelo.lopesdasilva@gggmail.com>
2017-12-22 14:59:26 -0200

    test

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var loginRequired = require('./helpers/loginRequired');
-var cors = require('cors');
 
 var app = express();
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
