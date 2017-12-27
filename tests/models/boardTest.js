7a32f0612a4f043815fc45e76383268d0d476558
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-12-27 10:33:15 -0200

    Change var to const

--- tests/models/boardTest.js
+++ tests/models/boardTest.js
@@ -1,37 +1,39 @@
-var chai = require('chai');
-var expect = chai.expect;
-var Board = require('./../../models/board/board');
-var FrameStatus = require('./../../models/board/frameStatus');
+"use strict";
+
+const chai = require('chai');
+const expect = chai.expect;
+const Board = require('./../../models/board/board');
+const FrameStatus = require('./../../models/board/frameStatus');
 
 describe('Board', function () {
   it('Characters must be in correct position', function () {
-    var board = Object.create(Board).init();
+    const board = Object.create(Board).init();
 
     expect(board.characterFrames[0].character.name).to.equal("Alfredo");
     expect(board.characterFrames[23].character.name).to.equal("Zeca");
   });
 
   it('Mistery face should not be equal in both boards all the times', function () {
-    var collision = 0;
+    let collision = 0;
 
     for (let i = 1; i <= 100; i++) {
-      var boardOne = Object.create(Board).init();
-      var boardTwo = Object.create(Board).init();
+      const boardOne = Object.create(Board).init();
+      const boardTwo = Object.create(Board).init();
 
-      if (boardOne.misteryFace.name == boardTwo.misteryFace.name) {
+      if (boardOne.misteryFace === boardTwo.misteryFace) {
         ++collision;
       }
 
       expect(collision).to.not.equal(100);
     }
-    
+
     console.log("Mistery face collision: " + collision + "%");
   });
 
   it('Flip Ana character frame', function () {
-    var board = Object.create(Board).init();
+    const board = Object.create(Board).init();
     const anaCharacterId = "CQEJBwwCCAY";
-    var frame = board.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
+    const frame = board.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
 
     board.flipCharacterFrame(anaCharacterId);
     expect(frame.status).to.equal(FrameStatus.down);
@@ -41,19 +43,19 @@ describe('Board', function () {
   });
 
   it('Counting board character frames', function () {
-    var boardOne = Object.create(Board).init();
-    var boardTwo = Object.create(Board).init();
+    const boardOne = Object.create(Board).init();
+    const boardTwo = Object.create(Board).init();
 
     expect(boardOne.characterFrames.length).to.equal(24);
     expect(boardTwo.characterFrames.length).to.equal(24);
   });
 
   it("Check if flipping a character in one board doesn't flip the same character in the other", function () {
-    var boardOne = Object.create(Board).init();
-    var boardTwo = Object.create(Board).init();
+    const boardOne = Object.create(Board).init();
+    const boardTwo = Object.create(Board).init();
     const anaCharacterId = "CQEJBwwCCAY";
-    var frameInBoardOne = boardOne.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
-    var sameFrameInBoardTwo = boardTwo.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
+    const frameInBoardOne = boardOne.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
+    const sameFrameInBoardTwo = boardTwo.characterFrames.find(function (cf) { return cf.character.id === anaCharacterId; });
 
     boardOne.flipCharacterFrame(anaCharacterId);
 
