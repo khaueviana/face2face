43f92c16796487d28db3792dc6444bc31ffd928e
guilherme.sampaio <guilherme.sampaio@concrete.com.br>
2018-01-02 21:02:07 -0200

    Adding image property to characterframe

--- models/board/characterFrame.js
+++ models/board/characterFrame.js
@@ -3,6 +3,7 @@ var FrameStatus = require("./frameStatus");
 var CharacterFrame = function(character) {
     this.character = character;
     this.status = FrameStatus.up;
+    this.image = "Alfredo.AQ0DDg0DCgo.png";
 };
 
 module.exports = CharacterFrame;
