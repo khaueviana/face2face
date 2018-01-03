fc519f45266c375b774ab6cb61aaf2b7a577b928
gustavo-segantini <gsrxyz@gmail.com>
2018-01-03 14:52:41 -0200

    FIx image name formation

--- models/board/characterFrame.js
+++ models/board/characterFrame.js
@@ -1,9 +1,9 @@
 var FrameStatus = require("./frameStatus");
 
-var CharacterFrame = function(character) {
+var CharacterFrame = function (character) {
     this.character = character;
     this.status = FrameStatus.up;
-    this.image = "Alfredo.AQ0DDg0DCgo.png";
+    this.image = `${character.name}.${character.id}.png`;
 };
 
 module.exports = CharacterFrame;
